b47476d7956adacfc3a6a0468620cd6d7d2ddee2
sijie.sun <sijie.sun@smartx.com>
2026-03-29 14:34:38 +0800

    fix permission

--- easytier/src/launcher.rs
+++ easytier/src/launcher.rs
@@ -403,12 +403,6 @@ impl NetworkInstance {
         self.config.get_network_identity().network_name
     }
 
-    pub fn set_tun_fd(&mut self, tun_fd: i32) {
-        if let Some(launcher) = self.launcher.as_ref() {
-            let _ = launcher.data.tun_fd.0.blocking_send(Some(tun_fd));
-        }
-    }
-
     pub fn get_tun_fd_sender(&self) -> Option<mpsc::Sender<TunFd>> {
         self.launcher
             .as_ref()
@@ -874,19 +868,8 @@ impl NetworkConfig {
         }
 
         let peers = config.get_peers();
-        match peers.len() {
-            1 => {
-                result.networking_method = Some(NetworkingMethod::PublicServer as i32);
-                result.public_server_url = Some(peers[0].uri.to_string());
-            }
-            0 => {
-                result.networking_method = Some(NetworkingMethod::Standalone as i32);
-            }
-            _ => {
-                result.networking_method = Some(NetworkingMethod::Manual as i32);
-                result.peer_urls = peers.iter().map(|p| p.uri.to_string()).collect();
-            }
-        }
+        result.networking_method = Some(NetworkingMethod::Manual as i32);
+        result.peer_urls = peers.iter().map(|p| p.uri.to_string()).collect();
 
         result.listener_urls = config
             .get_listeners()
